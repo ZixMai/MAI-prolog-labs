b3b28396db63fec6f2d1558286df756fa8efcf01
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-12-10 04:35:15 +0300

    feat: done lab3

--- lab3/task.pl
+++ lab3/task.pl
@@ -1,93 +1,81 @@
-exception('Koza', 'Volk').
-exception('Koza', 'Kapusta').
-exception('Volk', 'Koza').
-exception('Kapusta', 'Koza').
+bad(volk, koza).
+bad(koza, volk).
+bad(kapusta, koza).
+bad(koza, kapusta).
 
-write_list([]).
-write_list([H|T]) :-
-  format('~w~n', [H]),
-  write_list(T).
+checkIfBad([E1, E2]) :- bad(E1, E2).
 
 push_back(E, [], [E]).
 push_back(E, [H|T], [H|T1]) :- push_back(E, T, T1).
 
-check([Item1, Item2]) :- exception(Item1, Item2).
+print_list([]).
+print_list([A|T]) :- print_list(T), write(A), nl.
 
-move(s([Item1, Item2, Item3], 'L', []), s([Item1, Item2], 'R', [Item3])) :- not(check([Item1, Item2])).
-move(s([Item1, Item2, Item3], 'L', []), s([Item1, Item3], 'R', [Item2])) :- not(check([Item1, Item3])).
-move(s([Item1, Item2, Item3], 'L', []), s([Item2, Item3], 'R', [Item1])) :- not(check([Item2, Item3])).
+move(s([E1, E2, E3], 'L', []), s([E1, E2], 'R', [E3])) :- not(checkIfBad([E1, E2])).
+move(s([E1, E2, E3], 'L', []), s([E1, E3], 'R', [E2])) :- not(checkIfBad([E1, E3])).
+move(s([E1, E2, E3], 'L', []), s([E2, E3], 'R', [E1])) :- not(checkIfBad([E2, E3])).
 
-move(s([Left|T], 'R', Right), s([Left|T], 'L', Right)) :- not(check(Right)).
-move(s(Left, 'R', [Item1, Item2]), s(Out, 'L', [Item2])) :- check([Item1, Item2]), push_back(Item1, Left, Out).
+move(s([L|T], 'R', R), s([L|T], 'L', R)) :- not(checkIfBad(R)).
+move(s(L, 'R', [E1, E2]), s(RES, 'L', [E2])) :- checkIfBad([E1, E2]), push_back(E1, L, RES).
 
-move(s([L|LT], 'L', [R|RT]), s(LT, 'R', Out)) :- push_back(L, [R|RT], Out).
-move(s([X, L|LT], 'L', [R|RT]), s([X|LT], 'R', Out)) :- push_back(L, [R|RT], Out).
+move(s([L|LT], 'L', [R|RT]), s(LT, 'R', RES)) :- push_back(L, [R|RT], RES).
+move(s([X, L|LT],'L',[R|RT]), s([X|LT], 'R', RES)) :- push_back(L, [R|RT], RES).
 
-prolong([In|InT], [Out, In|InT]) :- move(In, Out), not(member(Out, [In|InT])).
-
-breadth([[B|T]|_], B, [B|T]).
-breadth([H|QT], X, R) :-
-    findall(Z, prolong(H, Z), T),
-    append(QT, T, OutQ), !,
-    breadth(OutQ, X, R).
-breadth([_|T], X, R) :- breadth(T, X, R).
+prolong([In1|In2], [RES, In1|In2]) :- move(In1, RES), not(member(RES, [In1|In2])).
 
 depth([X|T], X, [X|T]).
 depth(P, F, L) :- prolong(P, P1), depth(P1, F, L).
 
-depthIter([Finish|T], Finish, [Finish|T], 0).
-depthIter(Path, Finish, R, N) :-
-    N > 0,
-    prolong(Path, NewPath),
-    N1 is N - 1,
-    depthIter(NewPath, Finish, R, N1).
+breadth([[A|T]|_], A, [A|T]).
+breadth([H|T1], X, R) :- findall(Z, prolong(H, Z), T), append(T1, T, RES), !, breadth(RES, X, R).
+breadth([_|T], X, R) :- breadth(T, X, R).
 
-searchDepth(A, B) :-
+depthIter([F|T], F, [F|T], 0).
+depthIter(Path, F, R, N) :- N > 0, prolong(Path, Path1), N1 is N - 1, depthIter(Path1, F, R, N1).
+
+% DFS
+searchDFS(X, Y) :-
     get_time(DFSstart),
-    format('~n========================================~nsearchDepth START AT ~w~n========================================~n', [DFSstart]),
-    findall(
-        List,
-        (
-            get_time(StartTime),
-            depth([A], B, List),
-            get_time(EndTime),
-            write_list(List),
-            format('----------------------------------------~nsearchDepth END AT ~w~n', [EndTime]),
-            DFStime is EndTime - StartTime,
-            format('TIME IS ~w~n========================================~n', [DFStime])
-        ),
-        _
-    ).
+    depth([X], Y, L),
+    print_list(L),
+    get_time(DFSend),
+    DFStime is DFSend - DFSstart,
+    format('Time spent ~w~n========================================~n', [DFStime]).
 
-searchBreadth(X, Y) :-
-    get_time(DFSstart),
-    format('~n========================================~nsearchDepth START AT ~w~n========================================~n', [DFSstart]),
-    findall(
-        List,
-        (
-            get_time(StartTime),
-            breadth([[X]], Y, List),
-            get_time(EndTime),
-            write_list(List),
-            format('----------------------------------------~nsearchDepth END AT ~w~n', [EndTime]),
-            BFStime is EndTime - StartTime,
-            format('TIME IS ~w~n========================================~n', [BFStime])
-        ),
-        _
-    ).
+% BFS
+searchBFS(X, Y) :-
+    get_time(BFSstart),
+    breadth([[X]], Y, L),
+    print_list(L),
+    get_time(BFSend),
+    BFStime is BFSend - BFSstart,
+    format('Time spent ~w~n========================================~n', [BFStime]).
 
-searchIterDepth(Start, Finish) :-
-    TotalTime is 0,
+% Iterative
+searchIter(S, F) :-
     get_time(ITERstart),
-    format('~n========================================~nsearchId START AT ~w~n========================================~n', [ITERstart]),
-    between(1, inf, DepthLimit),
-    depthIter([Start], Finish, List, DepthLimit),
-    write_list(List),
+    between(1, 100, DepthLimit),
+    depthIter([S], F, Res, DepthLimit),
+    print_list(Res),
     get_time(ITERend),
-    format('----------------------------------------~nsearchId END AT ~w~n', [ITERend]),
-    T3 is ITERend - ITERstart,
-    format('TIME IS ~w~n========================================~n', [T3]),
-    TotalTime is TotalTime + T3,
-    write(TotalTime),
-    get_time(ITERstart).
+    ITERtime is ITERend - ITERstart,
+    format('Time spent ~w~n========================================~n', [ITERtime]).
 
+search :-
+    write('========================================'), nl,
+    get_time(T1),
+    findall(_, searchDFS(s([volk, koza, kapusta], 'L', []), s([], 'R', [_,_,_])), _),
+    get_time(T2),
+    DFStotalTime is T2 - T1,
+
+    get_time(T3),
+    findall(_, searchBFS(s([volk, koza, kapusta], 'L', []), s([], 'R', [_,_,_])), _),
+    get_time(T4),
+    BFStotalTime is T4 - T3,
+    get_time(T5),
+    findall(_, searchIter(s([volk, koza, kapusta], 'L', []), s([], 'R', [_,_,_])), _),
+    get_time(T6),
+    ITERtotalTime is T6 - T5,
+    format('Total DFS Time: ~w~n========================================~n', [DFStotalTime]),
+    format('Total BFS Time: ~w~n========================================~n', [BFStotalTime]),
+    format('Total iterative Time: ~w~n========================================~n', [ITERtotalTime]).
