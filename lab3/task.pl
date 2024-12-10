eb77f484a461dea964f224209c7b53c49735b72e
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-12-10 11:39:12 +0300

    feat: add depth limit

--- lab3/task.pl
+++ lab3/task.pl
@@ -1,9 +1,9 @@
-bad(volk, koza).
-bad(koza, volk).
-bad(kapusta, koza).
-bad(koza, kapusta).
+isDead(volk, koza).
+isDead(koza, volk).
+isDead(kapusta, koza).
+isDead(koza, kapusta).
 
-checkIfBad([E1, E2]) :- bad(E1, E2).
+listIsDead([E1, E2]) :- isDead(E1, E2).
 
 push_back(E, [], [E]).
 push_back(E, [H|T], [H|T1]) :- push_back(E, T, T1).
@@ -11,12 +11,12 @@ push_back(E, [H|T], [H|T1]) :- push_back(E, T, T1).
 print_list([]).
 print_list([A|T]) :- print_list(T), write(A), nl.
 
-move(s([E1, E2, E3], 'L', []), s([E1, E2], 'R', [E3])) :- not(checkIfBad([E1, E2])).
-move(s([E1, E2, E3], 'L', []), s([E1, E3], 'R', [E2])) :- not(checkIfBad([E1, E3])).
-move(s([E1, E2, E3], 'L', []), s([E2, E3], 'R', [E1])) :- not(checkIfBad([E2, E3])).
+move(s([E1, E2, E3], 'L', []), s([E1, E2], 'R', [E3])) :- not(listIsDead([E1, E2])).
+move(s([E1, E2, E3], 'L', []), s([E1, E3], 'R', [E2])) :- not(listIsDead([E1, E3])).
+move(s([E1, E2, E3], 'L', []), s([E2, E3], 'R', [E1])) :- not(listIsDead([E2, E3])).
 
-move(s([L|T], 'R', R), s([L|T], 'L', R)) :- not(checkIfBad(R)).
-move(s(L, 'R', [E1, E2]), s(RES, 'L', [E2])) :- checkIfBad([E1, E2]), push_back(E1, L, RES).
+move(s([L|T], 'R', R), s([L|T], 'L', R)) :- not(listIsDead(R)).
+move(s(L, 'R', [E1, E2]), s(RES, 'L', [E2])) :- listIsDead([E1, E2]), push_back(E1, L, RES).
 
 move(s([L|LT], 'L', [R|RT]), s(LT, 'R', RES)) :- push_back(L, [R|RT], RES).
 move(s([X, L|LT],'L',[R|RT]), s([X|LT], 'R', RES)) :- push_back(L, [R|RT], RES).
@@ -54,7 +54,7 @@ searchBFS(X, Y) :-
 % Iterative
 searchIter(S, F) :-
     get_time(ITERstart),
-    between(1, 100, DepthLimit),
+    between(1, 11, DepthLimit),
     depthIter([S], F, Res, DepthLimit),
     print_list(Res),
     get_time(ITERend),
